2ce6758dbe3455220402c0bcda04f32e788a8c1f
Brad Rosen <brosen@mdsol.com>
2014-12-30 14:52:00 -0500

    initial

---
+++ Angular4DotNet/Controllers/CoursesController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace Angular4DotNet.Controllers
+{
+    public class CoursesController : Controller
+    {
+        //
+        // GET: /Courses/
+
+        public ActionResult Index()
+        {
+            return View("Index", "", "you");
+        }
+
+    }
+}
