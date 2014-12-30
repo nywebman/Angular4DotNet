7d14c59b588505215d9f3654ce533b400bf75bc0
Brad Rosen <brosen@mdsol.com>
2014-12-30 15:26:44 -0500

    serialize json data

--- Angular4DotNet/Controllers/CoursesController.cs
+++ Angular4DotNet/Controllers/CoursesController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Web.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace Angular4DotNet.Controllers
 {
@@ -9,8 +11,27 @@ namespace Angular4DotNet.Controllers
 
         public ActionResult Index()
         {
-            return View("Index", "", "you");
+            return View("Index", "", GetSerializedCourseVM());
         }
 
+        public string GetSerializedCourseVM()
+        {
+            var courses = new[]
+            {
+                new CourseVm {Number = "CREA101", Name = "care of Creatures", Instructor = "r hagrid"},
+                new CourseVm {Number = "DARK502", Name = "defence of dark arts", Instructor = "Snape"},
+                new CourseVm {Number = "TRAN201", Name = "Transfiguration", Instructor = "Mcconagal"}
+            };
+            var settings = new JsonSerializerSettings {ContractResolver = new CamelCasePropertyNamesContractResolver()}; 
+            return JsonConvert.SerializeObject(courses, Formatting.None, settings);
+        }
+
+    }
+
+    public class CourseVm
+    {
+        public string Number { get; set; }
+        public string Name { get; set; }
+        public string Instructor { get; set; }
     }
 }
