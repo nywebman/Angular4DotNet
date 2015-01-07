8f47c7a8184b0ab1396721ea8bd4ced3860655d0
Brad Rosen <brosen@mdsol.com>
2015-01-07 14:42:37 -0500

    only works in earlier version?

---
+++ Angular4DotNet/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Angular4DotNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public void Index()
+        {
+            Response.Redirect(@"/Registration");
+        }
+
+    }
+}
